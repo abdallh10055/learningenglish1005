7d123bbbede541a44cef51c1d47e363cd2e1d1bf
عالم الإنجليزية <bdallhqrytm3@gmail.com>
2025-04-11 23:54:25 +0200

    Add files via upload
    add

---
+++ adjectives.js
@@ -0,0 +1,32 @@
+const adjectivesDictionary = {
+    "happy": {
+        "arabic_meaning": "سعيد"
+    },
+    "sad": {
+        "arabic_meaning": "حزين"
+    },
+    "big": {
+        "arabic_meaning": "كبير"
+    },
+    "small": {
+        "arabic_meaning": "صغير"
+    },
+    "fast": {
+        "arabic_meaning": "سريع"
+    },
+    "slow": {
+        "arabic_meaning": "بطيء"
+    },
+    "beautiful": {
+        "arabic_meaning": "جميل"
+    },
+    "ugly": {
+        "arabic_meaning": "قبيح"
+    },
+    "smart": {
+        "arabic_meaning": "ذكي"
+    },
+    "stupid": {
+        "arabic_meaning": "غبي"
+    }
+};
---
+++ adverbs.js
@@ -0,0 +1,32 @@
+const adverbsDictionary = {
+    "quickly": {
+        "arabic_meaning": "بسرعة"
+    },
+    "slowly": {
+        "arabic_meaning": "ببطء"
+    },
+    "happily": {
+        "arabic_meaning": "بسعادة"
+    },
+    "sadly": {
+        "arabic_meaning": "بحزن"
+    },
+    "loudly": {
+        "arabic_meaning": "بصوت عال"
+    },
+    "quietly": {
+        "arabic_meaning": "بهدوء"
+    },
+    "well": {
+        "arabic_meaning": "بشكل جيد"
+    },
+    "badly": {
+        "arabic_meaning": "بشكل سيء"
+    },
+    "easily": {
+        "arabic_meaning": "بسهولة"
+    },
+    "hard": {
+        "arabic_meaning": "بصعوبة"
+    }
+};
