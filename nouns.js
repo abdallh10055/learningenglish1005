563580b956d78234cb2d8f2526e7ba9da4a98aa0
عالم الإنجليزية <bdallhqrytm3@gmail.com>
2025-04-11 23:53:51 +0200

    nouns
    nouns

---
+++ nouns.js
@@ -0,0 +1,42 @@
+const nounsDictionary = {
+    "car": {
+        "arabic_meaning": "سيارة",
+        "definition": "A vehicle used for transportation on roads"
+    },
+    "house": {
+        "arabic_meaning": "منزل",
+        "definition": "A building for human habitation"
+    },
+    "book": {
+        "arabic_meaning": "كتاب",
+        "definition": "A written or printed work consisting of pages glued or sewn together"
+    },
+    "computer": {
+        "arabic_meaning": "حاسوب",
+        "definition": "An electronic device for storing and processing data"
+    },
+    "phone": {
+        "arabic_meaning": "هاتف",
+        "definition": "A device used to communicate with others over long distances"
+    },
+    "table": {
+        "arabic_meaning": "طاولة",
+        "definition": "A piece of furniture with a flat top and one or more legs"
+    },
+    "chair": {
+        "arabic_meaning": "كرسي",
+        "definition": "A separate seat for one person, typically with a back and four legs"
+    },
+    "door": {
+        "arabic_meaning": "باب",
+        "definition": "A hinged, sliding, or revolving barrier at the entrance to a building or room"
+    },
+    "window": {
+        "arabic_meaning": "نافذة",
+        "definition": "An opening in a wall or door fitted with glass to admit light"
+    },
+    "pen": {
+        "arabic_meaning": "قلم",
+        "definition": "An instrument for writing or drawing with ink"
+    }
+};
